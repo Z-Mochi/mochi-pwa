c7939a99e095fde78ae44e15f523a50530cdc994
z-mochi <zmochi2606@gmail.com>
2025-05-30 22:21:22 +0700

    feat: v4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-pwa-cache-v3';
+const CACHE_NAME = 'my-pwa-cache-v4';
 const FILES_TO_CACHE = [
   '/index.html',
   '/app.js',
@@ -39,16 +39,16 @@ self.addEventListener("activate", (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     (async () => {
-   
+
       const cachedResponse = await caches.match(event.request);
       if (cachedResponse) {
         return cachedResponse;
       }
-    // Attempt to get the preload response first
-    const preloadResponse = await event.preloadResponse;
-    if (preloadResponse) {
-      return preloadResponse;
-    }
+      // Attempt to get the preload response first
+      const preloadResponse = await event.preloadResponse;
+      if (preloadResponse) {
+        return preloadResponse;
+      }
 
       // Fallback to network fetch if no preload or cache is available
       return fetch(event.request);
